d310fb37c0ae881f95a4866f34708a8cff4ddae9
Daniel Armando Ciendua Ricartue <daciendua@uniboyaca.edu.co>
2026-03-02 02:18:26 -0500

    Probando la conexión al servidor

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000;
+
+app.use(express());
+
+app.listen(port,"localhost",()=>{
+    
+    console.log(`El servidor esta corriendo en el puerto: ${port}`); //Con el comando Ctrl C detiene el proceso del servidor
+});
